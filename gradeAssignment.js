8601be2b57b28aea8411776b88723b7245c53da5
ronronn <ronaldlegaspi960@gmail.com>
2025-03-28 11:11:19 +0800

    gradeAssignment.js

---
+++ gradeAssignment.js
@@ -0,0 +1,10 @@
+score =  float(input("enter your score:"))
+
+If score >= 90:
+   print("your grade is Excellent.")
+elif score >= 80:
+   print("your grade is good.")
+elif score>= 70:
+   print("your grade is fair.")
+else:
+   print("your grade is needs improvement.")
